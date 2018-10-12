83879adeef5a96c8531a4180f6131f9ef836afde
Kris Steinhoff <steinhof@umich.edu>
2018-10-12 13:10:49 -0400

    Update requirements

--- setup.py
+++ setup.py
@@ -7,12 +7,11 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'pandas==0.23.4',
-        'user-agents==1.1.0',
-        'netaddr==0.7.19',
-        'pendulum==1.4.0',
-        'scikit-learn==0.20.0',
-        'Flask==1.0.2',
         'Faker==0.9.1',
+        'Flask==1.0.2',
+        'netaddr==0.7.19',
+        'pandas==0.23.4',
+        'scikit-learn==0.20.0',
+        'user-agents==1.1.0',
     ],
 )
